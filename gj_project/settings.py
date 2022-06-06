66c7cc376fc101f18b1dbdb666b5295998f97ecc
neerajmaran1199 <52347137+neerajmaran1199@users.noreply.github.com>
2022-06-06 10:55:07 +0530

    Update settings.py

--- gj_project/settings.py
+++ gj_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dx@14u&dmp8nurn58=9+*e8t$i=*#d^)9ve*)6n8d4evuf0au2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
